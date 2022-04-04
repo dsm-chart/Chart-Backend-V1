b9becba065ac0f62e1896d8ca2e733d8b0c3d201
jinu <jinwoo794533@gmail.com>
2022-04-04 22:12:10 +0900

    ConfigurationPropertiesScan

--- src/main/kotlin/com/chart/chart/ChartApplication.kt
+++ src/main/kotlin/com/chart/chart/ChartApplication.kt
@@ -1,9 +1,11 @@
 package com.chart.chart
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ChartApplication
 
 fun main(args: Array<String>) {
