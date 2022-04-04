989391540518168a235b42d6ad65b52cc34c10fd
jinu <jinwoo794533@gmail.com>
2022-04-04 16:45:55 +0900

    init

---
+++ src/test/kotlin/com/chart/chart/ChartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chart.chart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
