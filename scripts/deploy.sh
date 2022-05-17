7e09a2a42892d161b4d48ff6d2d7cdb033b21224
jinu <jinwoo794533@gmail.com>
2022-05-18 00:15:22 +0900

    dd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,19 +7,7 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl jaewon-study | awk '{print $1}')
-
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
+sudo pkill -9 "java"
 
 echo "> 새 어플리케이션 배포"
 
