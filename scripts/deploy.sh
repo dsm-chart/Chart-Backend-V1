5e2f37e2694ec25a217e4dd89546ccf3d6b00ea1
jinu <jinwoo794533@gmail.com>
2022-05-17 21:43:05 +0900

    d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-REPOSITORY=/home/ec2-user
+REPOSITORY=/home/ec2-user/app/travis
+PROJECT_NAME=jaewon-study
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> Build 파일 복사"
 
-CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
+cp $REPOSITORY/build/build/libs/*.jar $REPOSITORY/jar
 
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl jaewon-study | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/jar/*.jar | tail -n 1)
 
-echo "> JAR NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
@@ -28,4 +33,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
