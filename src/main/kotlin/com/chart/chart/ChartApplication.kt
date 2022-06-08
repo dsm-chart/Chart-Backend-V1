ef9b1261640468b2df2c6f830f8ba19717aa53d7
jinu <jinwoo794533@gmail.com>
2022-06-08 23:35:48 +0900

    EnableJpaAuditing

--- src/main/kotlin/com/chart/chart/ChartApplication.kt
+++ src/main/kotlin/com/chart/chart/ChartApplication.kt
@@ -3,9 +3,11 @@ package com.chart.chart
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 class ChartApplication
 
 fun main(args: Array<String>) {
